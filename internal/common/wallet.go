43eabf19cb3a4d67128224addcb685a5642890c6
Jacob Smith <jacobsmithsinbox@gmail.com>
2025-12-14 22:21:49 -0700

    updated wallet

--- internal/common/wallet.go
+++ internal/common/wallet.go
@@ -4,27 +4,88 @@ package common
 // imports
 import (
 	"crypto/ed25519"
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
 )
-
-// interface definitions
-type IWallet interface{}
 
 // structure definitions
 type Wallet struct {
-	Public  ed25519.PublicKey
-	Private ed25519.PrivateKey
+	Name    string             `json:"name,omitempty"`
+	Public  ed25519.PublicKey  `json:"public"`
+	Private ed25519.PrivateKey `json:"private"`
 }
 
-// construct a wallet from a path
-func NewWallet(path string) (
-	wallet *Wallet,
-	err error,
-) {
+// construct a wallet
+func NewWallet() (*Wallet, error) {
 
 	// initialized data
-	err = nil
-	wallet = new(Wallet)
+	var err error = nil
+	var wallet *Wallet = new(Wallet)
+
+	// generate a key pair
+	wallet.Public, wallet.Private, err = ed25519.GenerateKey(nil)
+	if err != nil {
+		return nil, err
+	}
 
 	// done
-	return wallet, err
+	return wallet, nil
+}
+
+func (w Wallet) String() string {
+	return fmt.Sprintf("%s (%s)", w.Name, string(w.Public))
+}
+
+func (w Wallet) MarshalJSON() ([]byte, error) {
+
+	// initialized data
+	var publicKey string = base64.StdEncoding.EncodeToString(w.Public)
+	var privateKey string = base64.StdEncoding.EncodeToString(w.Private)
+
+	// done
+	return json.Marshal(struct {
+		Public  string `json:"public"`
+		Private string `json:"private"`
+	}{
+		Public:  publicKey,
+		Private: privateKey,
+	})
+}
+
+func (w *Wallet) UnmarshalJSON(data []byte) error {
+
+	// initialized data
+	var err error = nil
+	var publicBytes []byte
+	var privateBytes []byte
+	var transient struct {
+		Public  string `json:"public"`
+		Private string `json:"private"`
+	}
+
+	// unmarshall
+	err = json.Unmarshal(data, &transient)
+	if err != nil {
+		return err
+	}
+
+	// decode public key
+	publicBytes, err = base64.StdEncoding.DecodeString(transient.Public)
+	if err != nil {
+		return err
+	}
+
+	// decode private key
+	privateBytes, err = base64.StdEncoding.DecodeString(transient.Private)
+	if err != nil {
+		return err
+	}
+
+	// store the public and private keys
+	w.Public = ed25519.PublicKey(publicBytes)
+	w.Private = ed25519.PrivateKey(privateBytes)
+
+	// done
+	return nil
 }

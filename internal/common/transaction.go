5fa64c87f8e8f03a344f3a2b0f2f8482f30a7d7b
Alex <aptik.dev@gmail.com>
2025-12-14 22:22:08 -0700

    Sets up transaction primitive and Implements signing

---
+++ internal/common/transaction.go
@@ -0,0 +1,106 @@
+package common
+
+import (
+	"bytes"
+	"crypto/ed25519"
+	"encoding/binary"
+	"log"
+	"time"
+)
+
+const (
+	TYPESEND = iota
+	TYPEMINT
+	TYPEBURN
+	TYPESTORE
+)
+
+const MAX_TX_SIZE uint16 = 1232
+
+// Transaction definiton
+type Tx struct {
+	Data      TxData
+	Signature TxSig
+	//Size      []byte // size of the transaction in bytes
+}
+
+// Definition of data held within a transaction
+type TxData struct {
+	InvolvedAddresses TxAddresses // all addresses involved in a transaction
+	Instruction       TxInstruct  // instructions for transaction type
+	Hash              []byte      // hash of the transaction
+	Time              time.Time   // time that transaction was seen
+}
+
+// Defines fields based on transaction instruction
+type TxInstruct struct {
+	Type        uint8
+	Quantity    uint64
+	MessageData []byte
+}
+
+// Transaction signature definition
+type TxSig []byte
+
+// Transaction addresses definition
+type TxAddresses []ed25519.PublicKey
+
+type Transaction interface {
+
+	// Sign a transaction with a wallet
+	Sign(w *Wallet) []byte
+
+	// Verify a transaction's digital signature
+	Verify() bool
+
+	// Enforces that transaction fits protocol parameters
+	Enforce() bool
+
+	// Serialize a transaction into json
+	Serialize() []byte
+
+	// Send a transaction to mempool
+	MemSend(hosts ...interface{})
+}
+
+var transactionTypes = map[int]string{
+	TYPESEND: "Send",
+	//TypeMint: "Mint",
+	//TypeBurn: "Burn",
+	//TypeStore: "Store",
+}
+
+func (tx *Tx) Sign(w *Wallet) []byte {
+	signature := ed25519.Sign(w.Private, tx.Data.Instruction.encodeInstruction())
+	return signature
+}
+
+// Encode the Instructions to binary to prepare for signing
+func (txi *TxInstruct) encodeInstruction() []byte {
+	smallBuf := make([]byte, binary.MaxVarintLen64)
+	encodedInstruction := binary.PutUvarint(smallBuf, uint64(txi.Type))
+	encodedInstruction += binary.PutUvarint(smallBuf[encodedInstruction:], txi.Quantity)
+
+	largeBuf := new(bytes.Buffer)
+	lenBuf, err := largeBuf.Write(smallBuf)
+	if err != nil || lenBuf > 1232 {
+		log.Panicln("ERROR: Buffer too large or instructions over 1232 bytes")
+		return nil
+	}
+
+	messageLen := int64(len(txi.MessageData))
+	err = binary.Write(largeBuf, binary.BigEndian, messageLen)
+	if err != nil {
+		log.Println("ERROR: Transaction Message Data Length could not be written to buffer")
+		return nil
+	}
+
+	err = binary.Write(largeBuf, binary.BigEndian, txi.MessageData)
+	if err != nil {
+		log.Println("ERROR: Transaction Message Data could not be written to buffer")
+		return nil
+	}
+
+	return largeBuf.Bytes()
+
+}
--- internal/common/txn.go
+++
@@ -1,9 +0,0 @@
-package common
-
-import "crypto/ed25519"
-
-type txn struct {
-	rcv ed25519.PublicKey
-	snd ed25519.PublicKey
-	qty int
-}
